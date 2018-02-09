7b6c43af3490bc82474c9a867b5859b77247b64f
Lee <838826112@qq.com>
2018-02-09 15:59:35 +0800

    uu-cli

---
+++ src/template/component.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="component-name">
+    
+    
+    
+    </div>
+</template>
+
+<script>
+    
+    export default {
+        name: 'component-name',
+        data() {
+            return {}
+        },
+        filters: {},
+        methods: {},
+        mounted: function () {
+        },
+    }
+</script>
+
+<style lang="less">
+    .component-name {
+        
+    }
+</style>
